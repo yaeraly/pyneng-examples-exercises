c01bb7916effd2fd3d65acded48fd7099056c91a
e-coder-code <buildtechempire@gmail.com>
2019-10-20 10:45:02 +0000

    17.2 is done

--- exercises/17_serialization/task_17_2.py
+++ exercises/17_serialization/task_17_2.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+from pprint import pprint
 '''
 Задание 17.2
 
@@ -24,3 +26,24 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 '''
+#SW1              Eth 0/0           140          S I      WS-C3750-  Eth 0/1
+def parse_sh_cdp_neighbors(file_content):
+    regex = re.compile(r'(?P<l_device>\S+)[>#]'
+                       r'|(?P<r_device>\S+) +(?P<l_intf>\S+ \S+) +\d+.+\S+ +(?P<r_intf>\S+ \S+)')
+    result_dict = {}
+    for line in file_content.split('\n'):
+        match = regex.search(line)
+        if match and match.lastgroup == 'l_device':
+            l_dev = match.group('l_device')
+            result_dict[l_dev] = {}
+        elif match:
+            result_dict[l_dev][match.group('l_intf')] = {}
+            result_dict[l_dev][match.group('l_intf')][match.group('r_device')] = {}
+            result_dict[l_dev][match.group('l_intf')][match.group('r_device')] = match.group('r_intf')
+
+    return result_dict
+
+if __name__ in '__main__':
+    with open('sh_cdp_n_sw1.txt') as f:
+        pprint(parse_sh_cdp_neighbors(f.read()))
+
