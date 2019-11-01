d446c569cc89d02fb9be146bbf709a624e8095ec
yaeraly <ya.eraly@gmail.com>
2019-11-01 19:08:47 +0000

    19.2a is done

--- exercises/19_ssh_telnet/task_19_2a.py
+++ exercises/19_ssh_telnet/task_19_2a.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+import yaml
+from netmiko import ConnectHandler
+from paramiko.ssh_exception import AuthenticationException
+from netmiko.ssh_exception import NetMikoAuthenticationException
+from netmiko.ssh_exception import NetMikoTimeoutException
+
 '''
 Задание 19.2a
 
@@ -21,3 +27,33 @@ In [15]:
 
 Скрипт должен отправлять список команд commands на все устройства из файла devices.yaml с помощью функции send_config_commands.
 '''
+
+commands = [
+    'logging 10.255.255.1', 'logging buffered 20010', 'no logging console'
+]
+
+
+def send_config_commands(device, config_commands, verbose=True):
+    results = ''
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+            if verbose:
+                print(f'Connecting to {device["ip"]}')
+                results += ssh.send_config_set(config_commands)
+            else:
+                results += ssh.send_config_set(config_commands)
+
+    except (AuthenticationException, NetMikoAuthenticationException):
+        print('Authentication failure: unable to connect')
+    except (NetMikoTimeoutException):
+        print('Connection to device timed-out')
+    return results
+
+
+with open('devices.yaml') as f:
+    dict_device = yaml.safe_load(f)
+    result = send_config_commands(dict_device[0], commands, verbose=True)
+
+print(result)
+
