c4b7decb674788630a0f7e821742e56b35c2611c
yaeraly <ya.eraly@gmail.com>
2019-11-05 19:52:34 +0000

    20.1 is done

--- exercises/20_concurrent_connections/task_20_1.py
+++ exercises/20_concurrent_connections/task_20_1.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
 '''
 Задание 20.1
 
@@ -17,3 +20,32 @@
 
 Для проверки доступности IP-адреса, используйте ping.
 '''
+ip_addresses = ['192.168.100.1', '192.168.100.2', '192.168.100.4']
+
+
+def ping_ip(ip_address):
+    reply = subprocess.run(['ping', '-c', '1', '-n', f'{ip_address}'],
+                           stdout=subprocess.DEVNULL)
+
+    if reply.returncode == 0:
+        return True
+    else:
+        return False
+
+
+def ping_ip_addresses(ip_list, limit=3):
+    reach_ip = []
+    unreach_ip = []
+
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        results = executor.map(ping_ip, ip_list)
+        for ip, reply in zip(ip_list, results):
+            if reply is True:
+                reach_ip.append(ip)
+            else:
+                unreach_ip.append(ip)
+
+    return (reach_ip, unreach_ip)
+
+print(ping_ip_addresses(ip_addresses))
+
