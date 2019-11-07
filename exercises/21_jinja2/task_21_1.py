21c883c3a2adab3029c2051f7689382972fd8724
yaeraly <ya.eraly@gmail.com>
2019-11-07 19:50:27 +0000

    21.1 is done

--- exercises/21_jinja2/task_21_1.py
+++ exercises/21_jinja2/task_21_1.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+import yaml
+import os
+from sys import argv
+from jinja2 import Environment, FileSystemLoader
+
 '''
 Задание 21.1
 
@@ -13,3 +18,21 @@
 Проверить работу функции на шаблоне templates/for.txt и данных из файла data_files/for.yml.
 
 '''
+
+
+def generate_config(template, data_dict):
+    template_dir, template_file = os.path.split(template)
+
+    env = Environment(loader=FileSystemLoader(template_dir),
+                      trim_blocks=True,
+                      lstrip_blocks=True)
+    template = env.get_template(template_file)
+
+    return template.render(data_dict)
+
+
+if __name__ in "__main__":
+    with open(argv[2]) as f:
+        data = yaml.safe_load(f)
+
+    print(generate_config(argv[1], data))
